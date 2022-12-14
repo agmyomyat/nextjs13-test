ed96c2e78b98893ee065011bb39962c0de51f0e6
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-14 10:58:46 +0630

    CHANGE: ssr to static

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,27 +1,30 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-export default function Ssr({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-	return <div>{JSON.stringify(data)}</div>;
+// import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+// export default function Ssr({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+// 	return <div>{JSON.stringify(data)}</div>;
+// }
+// interface Data {
+// 	result: string;
+// }
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+// 	try {
+// 		const response = await fetch("https://r2.eiga.workers.dev");
+// 		const data: Data = await response.json();
+// 		console.log(data);
+// 		res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
+// 		return {
+// 			props: {
+// 				data,
+// 			},
+// 		};
+// 	} catch (e: any) {
+// 		console.log(e.message);
+// 		return {
+// 			props: {
+// 				error: e.message,
+// 			},
+// 		};
+// 	}
+// };
+export default function Ssr() {
+	return <div>hi</div>;
 }
-interface Data {
-	result: string;
-}
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-	try {
-		const response = await fetch("https://r2.eiga.workers.dev");
-		const data: Data = await response.json();
-		console.log(data);
-		res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
-		return {
-			props: {
-				data,
-			},
-		};
-	} catch (e: any) {
-		console.log(e.message);
-		return {
-			props: {
-				error: e.message,
-			},
-		};
-	}
-};
