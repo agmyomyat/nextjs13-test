a692f5428c92eb418ad727d873a49a825e0bfbe0
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-14 10:25:22 +0630

    init

---
+++ pages/ssr.tsx
@@ -0,0 +1,18 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+export default function Ssr({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	return <div>{JSON.stringify(data)}</div>;
+}
+interface Data {
+	result: string;
+}
+export const getServerSideProps: GetServerSideProps<{ data: Data }> = async ({ req, res }) => {
+	const response = await fetch("https://r2.eiga.workers.dev");
+	const data: Data = await response.json();
+	console.log(data);
+	res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
+	return {
+		props: {
+			data,
+		},
+	};
+};
