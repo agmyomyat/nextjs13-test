eb364cf5946e6e853b70055047437c3171bf9915
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-14 13:28:46 +0630

    ADD:kv api

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 export const config = {
 	runtime: "experimental-edge",
 };
+const kv_super = "sSgHIsMBIWb0FPWHSYUYPjsahcygS27idro7Nnf5";
 export default function Ssr(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	useEffect(() => {
 		console.log("prop", props);
@@ -14,19 +15,26 @@ interface Data {
 }
 export const getServerSideProps: GetServerSideProps = async context => {
 	try {
-		const kv = await (context as any).env.PIWARE.get("foo");
-
-		const response = await fetch("https://eu2-merry-lobster-30387.upstash.io/get/foo", {
-			headers: {
-				Authorization:
-					"Bearer AXazACQgY2NlNzFmOTMtMzFiNy00ODdkLTg0Y2UtNzI0MDNhZTFmMjA2ZjZjNzdlMjBmNGM1NGYxMmJhZDMzYjc1Yjk0MDE0NTA=",
-			},
-		});
-		const data: Data = await response.json();
-		console.log(data);
+		const kv = await fetch(
+			"https://api.cloudflare.com/client/v4/accounts/38b160f8095b196eabcc729f65528a08/storage/kv/namespaces/f45655ac40214bc4ae4fe149ca655438/values/foo",
+			{
+				headers: {
+					"X-Auth-Key": "304d80ba351e0c80e65fbb537a82e3b8fdded",
+					"X-Auth-Email": "aungmyomyat.sv@gmail.com",
+				},
+			}
+		);
+		// const response = await fetch("https://eu2-merry-lobster-30387.upstash.io/get/foo", {
+		// 	headers: {
+		// 		Authorization:
+		// 			"Bearer AXazACQgY2NlNzFmOTMtMzFiNy00ODdkLTg0Y2UtNzI0MDNhZTFmMjA2ZjZjNzdlMjBmNGM1NGYxMmJhZDMzYjc1Yjk0MDE0NTA=",
+		// 	},
+		// });
+		const response = await kv.text();
+		console.log(response);
 		return {
 			props: {
-				kv,
+				response,
 			},
 		};
 	} catch (e: any) {
