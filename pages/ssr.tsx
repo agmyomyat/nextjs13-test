9ff94be8f63ec05f4b710b6855d9fbc104ca136a
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-14 10:46:44 +0630

    ADD: try catch in ssr

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -6,13 +6,17 @@ interface Data {
 	result: string;
 }
 export const getServerSideProps: GetServerSideProps<{ data: Data }> = async ({ req, res }) => {
-	const response = await fetch("https://r2.eiga.workers.dev");
-	const data: Data = await response.json();
-	console.log(data);
-	res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
-	return {
-		props: {
-			data,
-		},
-	};
+	try {
+		const response = await fetch("https://r2.eiga.workers.dev");
+		const data: Data = await response.json();
+		console.log(data);
+		res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
+		return {
+			props: {
+				data,
+			},
+		};
+	} catch (e: any) {
+		throw Error(e.message);
+	}
 };
