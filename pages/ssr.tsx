9654c0a03de4ec4d8cd7b183b49c70fe12ff454d
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-14 11:22:49 +0630

    FIX: cloudflare runtime error

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,32 +1,32 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-export default function Ssr({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-	return <div>{JSON.stringify(data)}</div>;
-}
-interface Data {
-	result: string;
-}
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-	try {
-		const response = await fetch("https://r2.eiga.workers.dev");
-		const data: Data = await response.json();
-		console.log(data);
-		res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
-		return {
-			props: {
-				runtime: process.env.NEXT_RUNTIME,
-
-				data,
-			},
-		};
-	} catch (e: any) {
-		console.log(e.message);
-		return {
-			props: {
-				error: e.message,
-			},
-		};
-	}
-};
-// export default function Ssr() {
-// 	return <div>hi</div>;
+// export default function Ssr({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+// 	return <div>{JSON.stringify(data)}</div>;
 // }
+// interface Data {
+// 	result: string;
+// }
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+// 	try {
+// 		const response = await fetch("https://r2.eiga.workers.dev");
+// 		const data: Data = await response.json();
+// 		console.log(data);
+// 		res.setHeader("Cache-Control", "public, s-maxage=30, stale-while-revalidate=59");
+// 		return {
+// 			props: {
+// 				runtime: process.env.NEXT_RUNTIME,
+
+// 				data,
+// 			},
+// 		};
+// 	} catch (e: any) {
+// 		console.log(e.message);
+// 		return {
+// 			props: {
+// 				error: e.message,
+// 			},
+// 		};
+// 	}
+// };
+export default function Ssr() {
+	return <div>hi</div>;
+}
