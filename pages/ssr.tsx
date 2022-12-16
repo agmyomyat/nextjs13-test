d6deaddc4b5ea09fad0f07443a2bc0ea939b5f72
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-16 10:33:51 +0630

    ADD:try catch in redis init

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -9,9 +9,14 @@ import Redis from "ioredis";
 // 	password: "Ub3JEH1DI5frnfDDsTUQd2Y5oRZBMJ",
 // });
 // const redis = new Redis("redis://default:Ub3JEH1DI5frnfDDsTUQd2Y5oRZBMJ@tcp-mo2.mogenius.io:29504");
-const redis = new Redis(
-	"redis://default:f6c77e20f4c54f12bad33b75b9401450@eu2-merry-lobster-30387.upstash.io:30387"
-);
+let redis: Redis;
+try {
+	redis = new Redis(
+		"redis://default:f6c77e20f4c54f12bad33b75b9401450@eu2-merry-lobster-30387.upstash.io:30387"
+	);
+} catch (e: any) {
+	console.log(e.message);
+}
 // const redis = new Redis({
 // 	url: "https://eu2-merry-lobster-30387.upstash.io",
 // 	token: "AXazACQgY2NlNzFmOTMtMzFiNy00ODdkLTg0Y2UtNzI0MDNhZTFmMjA2ZjZjNzdlMjBmNGM1NGYxMmJhZDMzYjc1Yjk0MDE0NTA=",
