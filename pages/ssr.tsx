e9c1e8a982a4c97defdae431ae60ec59d14ab58d
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-12-16 10:27:28 +0630

    ADD: ioredis

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,11 +1,21 @@
 import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useEffect } from "react";
-import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
-	url: "https://eu2-merry-lobster-30387.upstash.io",
-	token: "AXazACQgY2NlNzFmOTMtMzFiNy00ODdkLTg0Y2UtNzI0MDNhZTFmMjA2ZjZjNzdlMjBmNGM1NGYxMmJhZDMzYjc1Yjk0MDE0NTA=",
-});
+import Redis from "ioredis";
+//default:Ub3JEH1DI5frnfDDsTUQd2Y5oRZBMJ@tcp-mo2.mogenius.io:29504
+// new Redis({
+// 	port: 29504, // Redis port
+// 	host: "tcp-mo2.mogenius.io", // Redis host
+// 	username: "default", // needs Redis >= 6
+// 	password: "Ub3JEH1DI5frnfDDsTUQd2Y5oRZBMJ",
+// });
+// const redis = new Redis("redis://default:Ub3JEH1DI5frnfDDsTUQd2Y5oRZBMJ@tcp-mo2.mogenius.io:29504");
+const redis = new Redis(
+	"redis://default:f6c77e20f4c54f12bad33b75b9401450@eu2-merry-lobster-30387.upstash.io:30387"
+);
+// const redis = new Redis({
+// 	url: "https://eu2-merry-lobster-30387.upstash.io",
+// 	token: "AXazACQgY2NlNzFmOTMtMzFiNy00ODdkLTg0Y2UtNzI0MDNhZTFmMjA2ZjZjNzdlMjBmNGM1NGYxMmJhZDMzYjc1Yjk0MDE0NTA=",
+// });
 export const config = {
 	runtime: "experimental-edge",
 };
@@ -30,7 +40,12 @@ export const getServerSideProps: GetServerSideProps = async context => {
 		// 		},
 		// 	}
 		// );
+		console.time("set time");
+		const set = await redis.set("foo", "amm"); // Returns a promise which resolves to "OK" when the command succeeds.
+		console.timeEnd("set time");
+		console.time("get time");
 		const response = await redis.get("foo");
+		console.timeEnd("get time");
 		console.log(response);
 		return {
 			props: {
